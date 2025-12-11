8aecefb720782e3cae252f7707ccc582932fa590
ownleex <145456737+ownleex@users.noreply.github.com>
2025-12-11 16:36:44 +0100

    first

--- backend/src/types/fastify-jwt.d.ts
+++
@@ -1,9 +0,0 @@
-import '@fastify/jwt';
-import { User } from './user';
-
-declare module '@fastify/jwt' {
-  interface FastifyJWT {
-    payload: Pick<User, 'id' | 'username'>;
-    user: User;
-  }
-}
